5a640c13a125decf7300b47c0d16c7b84adad708
Юлия Вороненко <juliavoronenko@macbook-air-ulia.local>
2018-11-03 22:32:01 +0300

    Урок третий, задание easy

--- lesson_03/home_work/hw03_easy.py
+++ lesson_03/home_work/hw03_easy.py
@@ -5,13 +5,40 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 
-# Задание-2:
+def my_round(number, pos):
+    origin_number_as_str = str(number)
+    numbers = origin_number_as_str.split(".")
+    first_part = numbers[0]
+    second_part = numbers[1]
+
+    result = first_part + '.' + second_part[:pos-1]
+
+    if int(second_part[pos+1]) >= 5:
+        result = result + str(int(second_part[pos-1]) + 1)
+    else:
+        result = result + second_part[pos]
+
+    return float(result)
+
+
+print(my_round(12.828794678765, 2))
+
+
+
+
+
+
+
+# Задание-2:q
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
-
+ticket = int(input("Введите шестизначный номер билета: "))
+f = lambda ticket : (lambda ticket : 'yes' if sum(ticket[:3]) == sum(ticket[3:]) else 'no')(list(map(int, list(str(ticket)))))
+res = f(ticket)
+print(res)
 
 
 
